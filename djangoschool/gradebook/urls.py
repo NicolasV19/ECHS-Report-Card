b2799ffbcba462d05c0b2310972ab47c74e3e2a1
Nicolas Vincent <154649444+NicolasV19@users.noreply.github.com>
2025-12-01 16:45:23 +0700

    Gradebook Updates
    views.py: logic multiform + penyimpanan data ke DB (akan diupdate lagi)
    forms.py: isi multiform + pengimplementasian formset untuk AssignmentDetail agar nama siswa dapat diiterate berdasarkan Course
    urls.py: URL GradeBook diubah karena memakai Class-based View
    beserta dengan migrasi

--- djangoschool/gradebook/urls.py
+++ djangoschool/gradebook/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.gb_index, name="gb-index"),
     path("teachers", views.teacher_list, name="teacher-list"),
     path("course", views.course_list, name="course-list"),
-    path("grade-entry", views.grade_entry, name="grade-entry"),
+    # path("grade-entry", views.grade_entry, name="grade-entry"),
+    path("grade-entry", views.GradeEntryForm.as_view(), name="grade-entry")
     ]
