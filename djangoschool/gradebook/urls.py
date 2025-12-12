98234925e4634e68f73368cdfb9f4227199680ee
Nicolas Vincent <154649444+NicolasV19@users.noreply.github.com>
2025-12-12 11:28:43 +0700

    Multiform Nilai Raport Siswa
    untuk URL halaman form raport

--- djangoschool/gradebook/urls.py
+++ djangoschool/gradebook/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("course", views.course_list, name="course-list"),
     # path("grade-entry", views.grade_entry, name="grade-entry"),
     path("grade-entry", views.GradeEntryForm.as_view(), name="grade-entry"),
-    path("attendance", views.attendance, name="student-attendance")
+    path("attendance", views.attendance, name="student-attendance"),
+    path("midterm_pdf", views.midterm_report, name="midterm_report"),
+    path("midterm_pdf_real/<int:student_id>/", views.midterm_report_pdf, name="midterm_report_pdf"),
+    path("report-card", views.ReportCardForm.as_view(), name="report-card")
+    # path("finished-screen", views.finished, name="finished-screen")
     ]
