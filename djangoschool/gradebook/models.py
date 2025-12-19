2d6e6b259282b413d3c6c828cbc8db2e9eb07385
tjrenk-cmyk <65383618+tjrenk-cmyk@users.noreply.github.com>
2025-12-19 09:48:05 +0700

    Add files via upload
    gradelevel table adjustment

--- djangoschool/gradebook/models.py
+++ djangoschool/gradebook/models.py
@@ -1,179 +1,154 @@
 from django.db import models
-from django.contrib.auth.models import User
+from datetime import datetime, date
+from admission.models import AcademicYear, AbstractClass, Student, SchoolLevel, GradeLevel, Teacher, LearningPeriod
+from simple_history.models import HistoricalRecords
+# from account.models import User
 
-
-# Create your models here.    
-
-
-class AbstractPerson(models.Model):
-    GENDER_CHOICES = {"M": "Male", "F": "Female"}
-    first_name = models.CharField(max_length=20)
-    middle_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    place_of_birth = models.CharField(max_length=25)
-    date_of_birth = models.DateField()
-    gender = models.CharField(max_length=1, blank=True, choices=GENDER_CHOICES)
-    class Meta:
-        abstract = True
-
-class Religion(models.Model):
-    religion_name = models.CharField(max_length=20)
+# Create your models here.
+GRADE_CHOICES = [
+    ("A", "A"),
+    ("B", "B"),
+    ("C", "C"),
+    ("D", "D"),
+    ("E", "E"),
+]
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=35, unique=True)
+    short_name = models.CharField(max_length=5, blank=True, null=True)
     def __str__(self):
-        return self.religion_name
+        return self.subject_name
 
-class Registration(AbstractPerson):
-    CONTACT_PREFS = {"W": "Whatsapp", "P": "Phone", "E": "Email"}
-    LAST_EDU = {"S3": "PhD", "S2": "Graduate", "S1": "Undergraduate", "Dipl": "Diploma", "SHS": "SMA", "JHS": "SMP", "OTH": "Other"}
-    form_no = models.CharField(max_length=20, unique=True)
-    nisn = models.CharField(max_length=20, unique=True, blank=True, null=True)
-    prev_school = models.CharField(max_length=50, blank=True, null=True)
-    prev_nis = models.CharField(max_length=20, blank=True, null=True)
-    birth_order = models.CharField(max_length=1, blank=True, null=True)
-    religion = models.ForeignKey(Religion, null=True, on_delete=models.CASCADE)
-    church_name = models.CharField(max_length=30, blank=True, null=True)
-    current_address = models.CharField(max_length=50, blank=True, null=True)
-    current_district = models.CharField(max_length=50, blank=True, null=True)
-    current_region = models.CharField(max_length=50, blank=True, null=True)
-    current_city = models.CharField(max_length=50, blank=True, null=True)
-    current_province = models.CharField(max_length=50, blank=True, null=True)
-    contact_whatsapp = models.CharField(max_length=20, blank=True, null=True)
-    contact_mobile = models.CharField(max_length=20, blank=True, null=True)
-    contact_email = models.CharField(max_length=20, blank=True, null=True)
-    contact_preference = models.CharField(max_length=1, blank=True, null=True, choices=CONTACT_PREFS)
-    mother_name = models.CharField(max_length=20, blank=True, null=True)
-    mother_nik = models.CharField(max_length=20, blank=True, null=True)
-    mother_religion = models.ForeignKey(Religion, null=True, on_delete=models.CASCADE, related_name="mother_religion")
-    mother_education = models.CharField(max_length=20, blank=True, choices=LAST_EDU)
-    mother_occupation = models.CharField(max_length=20, blank=True, null=True)
-    mother_address_same2applicant = models.BooleanField(default=True)
-    mother_address = models.CharField(max_length=50, blank=True, null=True)
-    mother_district = models.CharField(max_length=50, blank=True, null=True)
-    mother_region = models.CharField(max_length=50, blank=True, null=True)
-    mother_city = models.CharField(max_length=50, blank=True, null=True)
-    mother_province = models.CharField(max_length=50, blank=True, null=True)
-    mother_phone = models.CharField(max_length=20, blank=True, null=True)
-    mother_mobile = models.CharField(max_length=20, blank=True, null=True)
-    mother_whatsapp = models.CharField(max_length=20, blank=True, null=True)
-    mother_email = models.CharField(max_length=20, blank=True, null=True)
-    father_name = models.CharField(max_length=20, blank=True, null=True)
-    father_nik = models.CharField(max_length=20, blank=True, null=True)
-    father_religion = models.ForeignKey(Religion, null=True, on_delete=models.CASCADE, related_name="father_religion")
-    father_education = models.CharField(max_length=20, blank=True, choices=LAST_EDU)
-    father_occupation = models.CharField(max_length=20, blank=True, null=True)
-    father_address_same2applicant = models.BooleanField(default=True)
-    father_address = models.CharField(max_length=50, blank=True, null=True)
-    father_district = models.CharField(max_length=50, blank=True, null=True)
-    father_region = models.CharField(max_length=50, blank=True, null=True)
-    father_city = models.CharField(max_length=50, blank=True, null=True)
-    father_province = models.CharField(max_length=50, blank=True, null=True)
-    father_phone = models.CharField(max_length=20, blank=True, null=True)
-    father_mobile = models.CharField(max_length=20, blank=True, null=True)
-    father_whatsapp = models.CharField(max_length=20, blank=True, null=True)
-    father_email = models.CharField(max_length=20, blank=True, null=True)
-
+class AssignmentType(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    short_name = models.CharField(max_length=10)
     def __str__(self):
-        return f"{self.form_no} ({self.first_name})" 
+        return self.short_name
 
-class AcademicYear(models.Model):
-    year = models.CharField(max_length=4)
-    begin_date = models.DateField()
-    end_date = models.DateField()
-    def __str__(self):
-        return f"{self.year}"
-
-class LearningPeriod(models.Model):
-    academic_year = (models.ForeignKey(AcademicYear, on_delete=models.CASCADE))
-    period_name = models.CharField(max_length=15)
-    date_start = models.DateField()
-    date_end = models.DateField()
-    def __str__(self):
-        return f"{self.academic_year} / {self.period_name}"
-
-class Teacher(AbstractPerson):
-    join_date = (models.DateField())
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
-    def __str__(self):
-        return f"{self.last_name}, {self.first_name}"
-
-class Student(models.Model):
-    registration_data = models.OneToOneField(Registration, on_delete=models.CASCADE)
-    id_number = models.CharField(max_length=15, unique=True)
-    nisn = models.CharField(max_length=15, default="000000000000000", unique=True)
-    is_active = models.BooleanField(default=True)
-    na_date = models.DateField(null=True, blank=True)
-    na_reason = models.CharField(max_length=100, blank=True, null=True)
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['registration_data', 'nisn', 'is_active'],
-                                    name='unique_student_data'),
-        ]
-    def __str__(self):
-        return f"{self.id_number}"
-
-
-class AbstractClass(models.Model):
-    name = models.CharField(max_length=50)
-    short_name = models.CharField(max_length=6)
+class Weighting(models.Model):
     academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(Teacher, on_delete = models.CASCADE)
-    class Meta:
-        abstract = True
+    level = models.ForeignKey(GradeLevel, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    assignment = models.ForeignKey(AssignmentType, on_delete=models.CASCADE)
+    weight = models.DecimalField(max_digits=2, decimal_places=2, default=0.0)
 
-class Class(AbstractClass):
-    is_home_class = models.BooleanField(default=False)
+class Course(AbstractClass):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     def __str__(self):
-        return self.name
+        return self.short_name
 
-class ClassMember(models.Model):
-    kelas = models.ForeignKey(Class, on_delete=models.CASCADE)
+class CourseMember(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     na_date = models.DateField(null=True, blank=True)
     na_reason = models.CharField(max_length=100, blank=True, null=True)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['kelas', 'student', 'is_active'],
-                                    name='unique_class_members'),
-        ]
-
-    def __str__(self):
-        return f"{self.student}"
-
-class SchoolData(models.Model):
-    school_name = models.CharField(max_length=100, unique=True)
-    address1 = models.CharField(max_length=100)
-    address2 = models.CharField(max_length=100)
-    district1 = models.CharField(max_length=100)
-    district2 = models.CharField(max_length=100)
-    city_name = models.CharField(max_length=50)
-    province = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.school_name
-
-class SchoolLevel(models.Model):
-    level_name = models.CharField(max_length=25, unique=True)
-    short_name = models.CharField(max_length=4, unique=True, blank=True, null=True)
-    def __str__(self):
-        return self.level_name
-
-class GradeLevel(models.Model):
-    grade_name = models.CharField(max_length=25, unique=True)
-    short_name = models.CharField(max_length=4, unique=True, blank=True, null=True)
-    def __str__(self):
-        return self.grade_name
-
-class HeadMaster(models.Model):
-    school = models.ForeignKey(SchoolData, on_delete=models.CASCADE)
-    level = models.ForeignKey(SchoolLevel, default=1, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=100)
+class PassingGrade(models.Model):
+    academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    level = models.ForeignKey(GradeLevel, on_delete=models.CASCADE)
+    passing_grade = models.IntegerField()
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=["school", "level", "full_name"],
-                                    name="unique_head_masters")
+            models.UniqueConstraint(fields=['academic_year', 'subject', 'level'],
+                                    name='unique_passing_grades'),
         ]
 
+class GradeEntry(models.Model):
+    academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
+    period = models.ForeignKey(LearningPeriod, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    level = models.ForeignKey(GradeLevel, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    assignment_type = models.ForeignKey(AssignmentType, on_delete=models.CASCADE)
+    assignment_date = models.DateField(auto_now_add=True)
+    assignment_topic = models.CharField(max_length=100, default="to be fill later.")
+    # set table to readonly by disabling all save/delete methode
+
+    #def save(self, *args, **kwargs):
+    #    pass
+
+    #def delete(self, *args, **kwargs):
+    #    pass
+
+    #def __delete__(self, instance):
+    #    pass
+
+class AssignmentHead(models.Model):
+    assignment = models.ForeignKey(AssignmentType, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date = models.DateField(null=True, blank=True)
+    topic = models.TextField(null=True, blank=True)
+    max_score = models.IntegerField(default=100)
+
+class AssignmentDetail(models.Model):
+    assignment_head = models.ForeignKey(AssignmentHead, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    is_active = models.BooleanField(default=True)
+    na_date = models.DateField(null=True, blank=True)
+    na_reason = models.CharField(max_length=100, blank=True, null=True)
+
+class Rubric(models.Model):
+    RUBRIC_CHOICES = [("Spiritual", "Spiritual Behaviour"), ("Social", "Social Behaviour")]
+    academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=RUBRIC_CHOICES)
+    description = models.TextField(null=True, blank=True)
+    index = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = "Rubrics"
+        verbose_name = "Rubric"
+
     def __str__(self):
-        return f"{self.full_name}: {self.school} ({self.level})"
+        return self.description
+
+class RubricIndicator(models.Model):
+    rubric = models.ForeignKey(Rubric, on_delete=models.CASCADE)
+    indicator_text = models.TextField(null=True, blank=True, unique=True)
+    index = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = "Rubric Indicators"
+        verbose_name = "Rubric indicator"
+
+    def __str__(self):
+        return self.indicator_text
+
+class StudentReportcard(models.Model):
+    academic_year = models.ForeignKey(AcademicYear, on_delete=models.CASCADE)
+    period = models.ForeignKey(LearningPeriod, on_delete=models.CASCADE)
+    is_mid = models.BooleanField(default=False)
+    level = models.ForeignKey(GradeLevel, on_delete=models.CASCADE, related_name="students_level_reportcard")
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+class ReportcardGrade(models.Model):
+    reportcard = models.ForeignKey(StudentReportcard, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    final_score = models.IntegerField(default=0)
+    final_grade = models.CharField(max_length=1, choices=GRADE_CHOICES)
+    teacher_notes = models.TextField(null=True, blank=True)
+
+class StudentAttendance(models.Model):
+    ATTD_CHOICES = [("S", "Sick"), ("P", "Permit"), ("A", "Absent"), ("L", "Late")]
+    attendance_date = models.DateField(null=True, blank=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    attendance_type = models.CharField(max_length=1, choices=ATTD_CHOICES)
+    notes = models.TextField(null=True, blank=True)
+
+class ReportcardNotes(models.Model):
+    reportcard = models.ForeignKey(StudentReportcard, on_delete=models.CASCADE)
+    spiritual_char_statement = models.TextField(null=True, blank=True)
+    spiritual_char_notes = models.TextField(null=True, blank=True)
+    social_statement = models.TextField(null=True, blank=True)
+    social_char_notes = models.TextField(null=True, blank=True)
+    social_notes = models.TextField(null=True, blank=True)
+    homeroom_notes = models.TextField(null=True, blank=True)
+
+class StudentRubrics(models.Model):
+    SCORE_CHOICES = [(1,"1"), (2,"2"), (3,"3"), (4,"4")]
+    reportcard = models.ForeignKey(StudentReportcard, on_delete=models.CASCADE)
+    indicator = models.ForeignKey(RubricIndicator, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0, choices=SCORE_CHOICES)
